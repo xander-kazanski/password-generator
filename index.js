28869262d226f369cb1847c2c8960a5177d91286
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-11-28 13:37:36 -0500

    fix deploy

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+const generateBtn = document.getElementById("generate-btn");
+
+generateBtn.addEventListener('click', generatePasswords)
+
 let length = 15
 
 function getRandomCharacter() {
@@ -9,7 +13,7 @@ function getRandomCharacter() {
 }
 
 function generatePassword() {
-    password = "";
+    let password = "";
     
     for (let i = 0; i < length; i++) {
         password += getRandomCharacter()
